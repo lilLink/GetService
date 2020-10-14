c8c54d9cffbe2eae1f2f9e93bd689d1c3eada10f
NEWjers <serjsonet@gmail.com>
2020-10-14 17:28:30 +0300

    Working environment

---
+++ GetService/src/test/java/com/shukary/GetService/GetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shukary.GetService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
